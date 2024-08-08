12769359921ac19a5859499d203709f5653218f4
AlonsoVA <128510867+AlonsoVA@users.noreply.github.com>
2024-08-08 04:20:28 -0600

    correciones

--- app/index.js
+++ app/index.js
@@ -18,7 +18,7 @@ server.use(bodyParser.json());
 export const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Alonso.12vz',
     database: 'Av_image'
 });
 
