fe57df3ab29ce6aa9a8a5618aca9da06d1931ef5
Astral890 <enac7125@outlook.com>
2024-08-08 20:45:27 -0600

    Respaldo por si acaso

--- app/index.js
+++ app/index.js
@@ -18,7 +18,7 @@ server.use(bodyParser.json());
 export const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Alonso.12vz',
+    password: 'root',
     database: 'Av_image'
 });
 
@@ -243,5 +243,3 @@ server.post('/getUser',(req,res)=>{
         }
     })
 })
-
-server.get
