e164abea50fbb852155b6fd6bef9b51cfaaf6e03
Astral890 <enac7125@outlook.com>
2024-08-13 19:59:26 -0600

    Detallitos

--- app/index.js
+++ app/index.js
@@ -31,7 +31,7 @@ server.use(cors());
 server.get("/",(req,res)=>res.sendFile(_dirname+"/Hompage/Index.html"))
 server.get("/cop",(req,res)=>res.sendFile(_dirname+"/Hompage/Index copy.html"))
 server.get("/copingles",(req,res)=>res.sendFile(_dirname+"/Hompage/Index inglesuser.html"))
-server.get("/copinglesnotuser",(req,res)=>res.sendFile(_dirname+"/Hompage/Index notuser.html"))
+server.get("/copen",(req,res)=>res.sendFile(_dirname+"/Hompage/Index notuser.html"))
 server.get("/gal",(req,res)=>res.sendFile(_dirname+"/Layout/Pantalla-1.html"))
 server.get("/casual",(req,res)=>res.sendFile(_dirname+"/Casualpantalla/Casual.html"))
 server.get("/fav",(req,res)=>res.sendFile(_dirname+"/Favoritos/Fav.html"))
